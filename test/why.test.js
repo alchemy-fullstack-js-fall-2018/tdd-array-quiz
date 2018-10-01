709febb9eae01d361871b6b4d0006dbe740ce197
Jack <ajackofsometrades@gmail.com>
2018-10-01 15:54:48 -0700

    test 2 passing, refactored test 1 to work with modification to function

--- test/why.test.js
+++ test/why.test.js
@@ -3,16 +3,14 @@ const why = require('../lib/why');
 
 describe('why tests', () => {
 
+    const attempt = why(['dog', 'doggy', 'Yes', 'No']);
 
     it('removes words with y and Y', () => {
-        const attempt = why(['dog', 'doggy', 'Yes', 'No']);
-        const expected = ['dog', 'No'];
-        assert.deepEqual(attempt, expected);
+        assert.ok(!attempt.includes('doggy'));
     });
     
     it('replaces acceptable words with a question including the word', () => {
-        const attempt = why(['dog', 'doggy', 'Yes', 'No']);
-        const expected = ['Is it dog?', 'Is it No?'];
+        let expected = ['Is it dog?', 'Is it No?'];
         assert.deepEqual(attempt, expected);
     });
 
