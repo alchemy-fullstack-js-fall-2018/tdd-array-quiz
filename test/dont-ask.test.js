e18ddc018262d25dc70a0feabe4af9f8575ba9db
MikeBLambert <mlamb128@gmail.com>
2018-10-01 16:00:42 -0700

    Removes lowercase y

--- test/dont-ask.test.js
+++ test/dont-ask.test.js
@@ -4,9 +4,9 @@ const Replacer = require('../lib/dont-ask.js');
 describe('replacer', () => {
 
     it('removes words with "y" from array', done => {
+        const replacer = new Replacer([]);
+        
         const arr = ['sky', 'hot', 'Yeti', 'green'];
-        const replacer = new Replacer(arr);
-
         replacer.remove(arr, (err, newArr) => {
             if(err) return done(err);
             assert.deepEqual(arr, newArr);
