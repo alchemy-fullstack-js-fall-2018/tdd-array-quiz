b666f574fcd988f3f88b23a71536f1810298744c
Sophie Graham <sophiergraham1@gmail.com>
2018-10-01 16:05:48 -0700

    started words.js test-- not passing travis, not complete

---
+++ test/words.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const { word } = require('../lib/words');
+
+
+
+describe('word', () => {
+    newWord= [];
+    it('only keeps words with Y and X', () => {
+        const word= words('Yeti', 'Green');
+        assert.equal('Yeti');
+    });
+    it('word to phrase', () => {
+    
+      
+    });
+});
