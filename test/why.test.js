0e6f4d17b6a02cfff7fbf55cf1e9ec0f3a392bb7
KLPainter <KLPainter@gmail.com>
2018-10-01 15:59:02 -0700

    changed test description

--- test/why.test.js
+++ test/why.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const why = require('../lib/why');
 
-it('returns values in example', () => {
+it('returns values as described in example', () => {
     const input = ['sky', 'hot', 'Yeti', 'green'];
     const expected = ['Is it hot?', 'Is it green?'];
     const output = why(input);
