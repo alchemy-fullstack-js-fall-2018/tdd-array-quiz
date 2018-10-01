15933b3bde4db95ae9efe79e5ddcea4fc7f31688
Jack <ajackofsometrades@gmail.com>
2018-10-01 15:50:47 -0700

    failing second for changing words to questions

--- test/why.test.js
+++ test/why.test.js
@@ -5,9 +5,14 @@ describe('why tests', () => {
 
 
     it('removes words with y and Y', () => {
-
         const attempt = why(['dog', 'doggy', 'Yes', 'No']);
         const expected = ['dog', 'No'];
+        assert.deepEqual(attempt, expected);
+    });
+    
+    it('replaces acceptable words with a question including the word', () => {
+        const attempt = why(['dog', 'doggy', 'Yes', 'No']);
+        const expected = ['Is it dog?', 'Is it No?'];
         assert.deepEqual(attempt, expected);
     });
 
