a584f6ff6ad77af6c13aa42687c666414acf0599
Davidchhing <davidhchhing@gmail.com>
2018-10-01 15:56:20 -0700

    created list constructor and part of test

--- test/array.test.js
+++ test/array.test.js
@@ -1,2 +1,18 @@
 const assert = require('assert');
-const List = require('../lib/array')
+const List = require('../lib/array');
+
+describe('this should remove any word with letters Y or y', () => {
+    
+    let list;
+
+    beforeEach(() => {
+        list = new List(['yellow, Yell, apple, green, mighty']);
+    });
+
+    it('this should return a new list array without words including y', () => {
+        const mapResults = list.map(item => {
+            
+        });
+        assert.deepEqual(mapResults, []);
+    });
+});
