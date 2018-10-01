59bad1f42580755d6c794d4826d80c4c848f28ab
Ryan Luras <ryanluras@gmail.com>
2018-10-01 15:42:33 -0700

    initial commit

---
+++ test/quiz.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const quiz = require('../lib/quiz');
+
+describe('quiz', () => {
+
+    it('removes Y items from array and returns "is it + item" to the remainder.', () => {
+        const result = quiz([['sky', 'hot', 'Yeti', 'green']]);
+        assert.deepEqual(result, ['Is it hot?', 'Is it green?']);
+    });
+});
