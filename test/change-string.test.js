e57a3c36ddb238565d402cbdb284e41905dd4fa3
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-01 16:16:10 -0700

    passing

--- test/change-string.test.js
+++ test/change-string.test.js
@@ -5,11 +5,10 @@ describe('Change String', () => {
     
     it('removes words with y in them from an array and returns and array of the remaining words in this format: "Is it &lt;word&gt;?"', done => {
         const words = ['sky', 'hot', 'Yeti', 'green'];
-        changeString(words, (err, phrases) => {
-            if(err) return done(err);
-
+        changeString(words, (phrases) => {
             const expected = ['Is it hot?', 'Is it green?'];
             assert.deepEqual(phrases, expected);
+            done();
         });
     });
 
