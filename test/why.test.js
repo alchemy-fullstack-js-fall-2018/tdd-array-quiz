f31e8a7373dc9500e1e25136b4f666a2b5d05d98
KLPainter <KLPainter@gmail.com>
2018-10-01 15:46:13 -0700

    first test failing

---
+++ test/why.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const why= require('../lib/why');
+
+it('returns values in example', () => {
+    const input = ['sky', 'hot', 'Yeti', 'green'];
+    const expected = ['Is it hot?', 'Is it green?'];
+    const output = why(input);
+    assert.deepEqual(output, expected);
+});
